013261702ea85feae3a0c92fb5cc2a77327893d5
generous-vector-9581 <ojappah@gmail.com>
2019-11-28 02:11:55 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,3 +1,15 @@
+-- Section 1: schema.sql
+
+-- Build out the schema for our Fantasy Library database:
+
+-- All tables must have a PRIMARY KEY on the id
+-- Series have a title and belong to an author and a sub-genre
+-- A Sub-Genre has a name
+-- Authors have a name
+-- Books have a title and year and belong to a series
+-- Characters have a name, motto, and species and belong to an author and a series
+-- Books have many characters and characters are in many books in a series. How do we accomplish this complex association? With a join table between Characters and Books. This join table (let's call it character_books) will just have -in addition to its primary key- two foreign key columns for the character and book ids. Each row in this join table acts as a relation between a book and a character.
+
 CREATE TABLE series (
   id INTEGER PRIMARY KEY,
   title TEXT,
